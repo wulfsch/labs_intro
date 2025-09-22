3d268edafe8ed946cff170fa7c6a1fbde041a036
Killian-Conyngham <killianconyngham@gmail.com>
2025-09-22 11:21:08 +0200

    fixing lotr project example

---
+++ session-03-automation/lotr_project/scripts/Makefile.R
@@ -0,0 +1,11 @@
+## clean out any previous work
+outputs <- c("./session-03-automation/lotr_project/data/raw/lotr_raw.tsv",
+             "./session-03-automation/lotr_project/data/processed/lotr_clean.tsv",
+             list.files(path =  "./session-03-automation/lotr_project/outputs/figures/",pattern = "*.png$"))
+file.remove(outputs)
+
+## run scripts
+source("./session-03-automation/lotr_project/scripts/00-packages.R")
+source("./session-03-automation/lotr_project/scripts/01-download-data.R")
+source("./session-03-automation/lotr_project/scripts/02-process-data.R")
+source("./session-03-automation/lotr_project/scripts/03-plot.R")
